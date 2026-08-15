4966678492f16a91c2126a597a76aa48cd9cee94
Anthony Toro <AToro14@users.noreply.github.com>
2026-08-14 20:21:40 -0700

    Add bash script to auto create new lesson folder with named files

---
+++ new_dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Use ./new_dir.sh EXISTING_CHAPTER_DIR_NAME NEW_LESSON_DIR_NAME
+CHAPTER_DIR=${1}
+LESSON_DIR=${2}
+
+cd /c/Users/Anthony/Desktop/Code/boot.dev/learn-go
+cd ${CHAPTER_DIR}
+cp -r ../template ${LESSON_DIR}/
+cd ${LESSON_DIR}
+mv temp.go ${LESSON_DIR}.go
+mv temp_test.go ${LESSON_DIR}_temp.go
+
