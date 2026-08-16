5fd9b2f39dd1a25294e2b1ba6945c11ed6fcf2bd
Anthony Toro <AToro14@users.noreply.github.com>
2026-08-15 19:04:15 -0700

    Complete Chapter 6: Lesson 10

---
+++ ch6-interfaces/message-formatter/message-formatter.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type formatter interface {
+	format() string
+}
+
+type plainText struct {
+	message string
+}
+
+func (pt plainText) format() string {
+	return pt.message
+}
+
+type bold struct {
+	message string
+}
+
+func (b bold) format() string {
+	return fmt.Sprintf("**%s**", b.message)
+}
+
+type code struct {
+	message string
+}
+
+func (c code) format() string {
+	return fmt.Sprintf("`%s`", c.message)
+}
+
+// Don't Touch below this line
+
+func sendMessage(format formatter) string {
+	return format.format() // Adjusted to call Format without an argument
+}
