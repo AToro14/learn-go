7ccadbd88f4ed10807c10ba81ffa95bb1b929768
Anthony Toro <AToro14@users.noreply.github.com>
2026-08-19 19:44:53 -0700

    Complete Chapter 8: Lesson 4

---
+++ ch8-loops/fizzbuzz.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func fizzbuzz() {
+	for i := 1; i <= 100; i++ {
+		output := ""
+		if i % 3 == 0 {
+			output += "fizz"
+		}
+		if i % 5 == 0 {
+			output += "buzz"
+		}
+		if len(output) == 0 {
+			fmt.Println(i)
+		} else {
+			fmt.Println(output)
+		}
+	}
+}
+
+func main() {
+	fizzbuzz()
+}
