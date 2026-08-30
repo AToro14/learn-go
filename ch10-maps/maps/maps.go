3a289b61297a4392d0a529c4bfaae2616a514b32
Anthony Toro <AToro14@users.noreply.github.com>
2026-08-29 20:18:35 -0700

    Improve Chapter 10: Lesson 1

--- ch10-maps/maps/maps.go
+++ ch10-maps/maps/maps.go
@@ -4,13 +4,11 @@ import "errors"
 
 func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
 	usersMap := make(map[string]user)
-	users := []user{}
 	if len(names) != len(phoneNumbers) {
 		return usersMap, errors.New("invalid sizes")
 	}
 	for i := 0; i < len(names); i++ {
-		users = append(users, user{name: names[i], phoneNumber: phoneNumbers[i]})
-		usersMap[users[i].name] = users[i]
+		usersMap[names[i]] = user{name: names[i], phoneNumber: phoneNumbers[i]}
 	}
 	return usersMap, nil
 }
