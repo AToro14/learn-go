3f3dd0ea8f2e8137ce194cb29a4bd58cd9900a61
Anthony Toro <AToro14@users.noreply.github.com>
2026-09-02 19:48:49 -0700

    Improve Chapter 10: Lesson 7

--- ch10-maps/distinct-words/distinct-words.go
+++ ch10-maps/distinct-words/distinct-words.go
@@ -5,13 +5,9 @@ import "strings"
 func countDistinctWords(messages []string) int {
 	distinctWords := make(map[string]struct{})
 	for _, sentence := range messages {
-		words := strings.Fields(sentence)
+		words := strings.Fields(strings.ToLower(sentence))
 		for _, word := range words {
-			// In this case the check below isn't necessary before inserting
-			// because we are only storing an empty struct
-			if _, ok := distinctWords[strings.ToLower(word)]; !ok {
-				distinctWords[strings.ToLower(word)] = struct{}{}
-			}
+			distinctWords[word] = struct{}{}
 		}
 	}
 	return len(distinctWords)
